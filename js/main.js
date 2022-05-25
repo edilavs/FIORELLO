c0584822162f47454116eec9ff01e8e73c837b9d
unknown <7nuv1d1@code.edu.az>
2022-05-25 17:09:11 +0400

    added smthing

--- js/main.js
+++ js/main.js
@@ -3,3 +3,10 @@ $(function() {
         $(".hover-list");
     })
 })
+var $grid = $('.grid').isotope({
+
+});
+$('.filter-button-group').on('click', 'li', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+});
